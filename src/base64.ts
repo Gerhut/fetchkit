8148410d37463364002732d2faf3d29eae94c669
George Cheng <Gerhut@GMail.com>
2022-05-14 11:20:54 +0000

    Better base64

--- src/base64.ts
+++ src/base64.ts
@@ -1,9 +1,8 @@
-export function base64(data: string) {
-  if (typeof btoa === "function") {
-    return btoa(data);
-  } else if (typeof Buffer === "function") {
-    return Buffer.from(data).toString("base64");
-  } else {
-    throw new Error("Unable to base64 data");
-  }
-}
+export const base64 =
+  typeof btoa === "function"
+    ? (data: string) => btoa(data)
+    : typeof Buffer === "function"
+    ? (data: string) => Buffer.from(data).toString("base64")
+    : (() => {
+        throw new Error("Unable to base64 data");
+      })();
